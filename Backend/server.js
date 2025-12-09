b72125041319aeeb74fc0cdb2ae20b71018ef610
Sriram S <ssriramcse23@gmail.com>
2025-12-09 14:34:51 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -163,7 +163,4 @@ app.post("/marks", async (req, res) => {
   }
 });
 
-// ---------------- START SERVER ----------------
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server running on port ${process.env.PORT || 5000}`)
-);
+
